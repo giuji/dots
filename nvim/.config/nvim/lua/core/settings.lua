f8c17ce277d162fc46dfbffa71cbb5af54968f34
giuji <64275280+giuji@users.noreply.github.com>
2024-03-10 14:40:00 +0100

    added lsp, treesitter

--- nvim/.config/nvim/lua/core/settings.lua
+++ nvim/.config/nvim/lua/core/settings.lua
@@ -1,11 +1,27 @@
 vim.opt.errorbells = false
 vim.opt.linebreak = true
 vim.opt.relativenumber = true
+
 vim.opt.termguicolors = true
+
 vim.opt.smartcase = true
--- vim.opt.undodir = '~/.cache/vimundo'
--- vim.opt.undofile = true
+
+-- undo
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.nvim/undodir"
+vim.opt.undofile = true
+
+-- tab
 vim.opt.autoindent = true
-vim.opt.expandtab = trye
+vim.opt.expandtab = true
 vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
+vim.opt.smartindent = true
+
+-- search
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.scrolloff = 5
